5ff6c8b15c158a11e386570dbf179e051637345c
joeelguerrero <123159442+joeelguerrero@users.noreply.github.com>
2023-05-05 08:43:43 +0000

    asf

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ btn.addEventListener('click', function() {
     btn.textContent = 'Modo oscuro';
   }
 });
-  function actualizarReloj() {
-    var fecha = new Date();
-    var hora = fecha.getHours();
-    var minutos = fecha.getMinutes();
-    var segundos = fecha.getSeconds();
-    hora = hora < 10 ? "0" + hora : hora;
-    minutos = minutos < 10 ? "0" + minutos : minutos;
-    segundos = segundos < 10 ? "0" + segundos : segundos;
-    var horaCompleta = hora + ":" + minutos + ":" + segundos;
-    document.getElementById("reloj").innerHTML = horaCompleta;
-  }
-  setInterval(actualizarReloj, 1000);
 
+  var time = [, , , ];
+  var fecha = new Date();
+  var reloj = document.getElementById('reloj');
+  time[0] = fecha.getHours();
+  time[1] = fecha.getMinutes();
+  time[2] = fecha.getSeconds();
+  var id = setInterval(update, 1000);
+  function update() {
+      fecha = new Date();
+      time[0] = modtime(fecha.getHours());
+      time[1] = modtime(fecha.getMinutes());
+      time[2] = modtime(fecha.getSeconds());
+      reloj.innerHTML = '<span class="square" id="h">' + time[0] + '</span><span class="point">:</span><span class="square" id="m">' + time[1] + '</span><span class="point">:</span><span class="square" id="s">' + time[2] + '</span>';
+  }
+  function modtime(hora) {
+      return (hora < 10 ? '0' + hora : hora);
+  }
   
