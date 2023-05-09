7937820293eb14a41c38c6666a73d43da49b84c9
joeelguerrero <123159442+joeelguerrero@users.noreply.github.com>
2023-05-09 07:09:02 +0000

    jh

--- script.js
+++ script.js
@@ -10,53 +10,60 @@ btn.addEventListener('click', function() {
   }
 });
 
-//Reloj
 function currentTime() {
-	var date = new Date();
-	var currentHour = date.getHours();
-	var currentMinute  = date.getMinutes();
-	var currentSecond = date.getSeconds();
-	var typeHour = (currentHour < 12) ? "AM" : "PM";
-	var currentSecond  = ((currentSecond < 10) ? "0" : "") + currentSecond;
-	var currentMinute  = ((currentMinute < 10) ? "0" : "") + currentMinute;
-	var currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
-	var currentHour = ((currentHour < 10) ? "0" : "")  + currentHour;
-	var fullTime = (currentHour + ":" + currentMinute + ":" + currentSecond + " " + typeHour);
-	document.getElementById('currentTime').innerHTML = fullTime;
+  var date = new Date();
+  var currentHour = date.getHours();
+  var currentMinute = date.getMinutes();
+  var currentSecond = date.getSeconds();
+  var typeHour = currentHour < 12 ? 'AM' : 'PM';
+  currentSecond = (currentSecond < 10 ? '0' : '') + currentSecond;
+  currentMinute = (currentMinute < 10 ? '0' : '') + currentMinute;
+  currentHour = currentHour > 12 ? currentHour - 12 : currentHour;
+  currentHour = (currentHour < 10 ? '0' : '') + currentHour;
+  var fullTime =
+    currentHour + ':' + currentMinute + ':' + currentSecond + ' ' + typeHour;
+  document.getElementById('currentTime').innerHTML = fullTime;
 }
 
 setInterval(currentTime, 1000);
 
-//Alarma
-setInterval( function() {
-	var date = new Date();
-	var currentHour = date.getHours();
-	var currentMinute  = date.getMinutes();
-	var currentSecond = date.getSeconds();
-	var typeHour = (currentHour < 12) ? "AM" : "PM";
-	var currentSecond  = ((currentSecond < 10) ? "0" : "") + currentSecond;
-	var currentMinute  = ((currentMinute < 10) ? "0" : "") + currentMinute;
-	var currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
-	var currentHour = ((currentHour < 10) ? "0" : "")  + currentHour;
-	var fullTime = (currentHour + ":" + currentMinute + ":" + currentSecond + " " + typeHour);
-	document.getElementById('currentTimeAlarm').innerHTML = fullTime;
-		// si la hora actual coincide con la hora indicada...
-		if( currentHour == document.getElementById("hour").value &&
-			currentMinute == document.getElementById("minutes").value &&
-			currentSecond == document.getElementById("seconds").value &&
-		   	typeHour == document.getElementById("type").value
-		)
-		{
-			var audio = new Audio('http://ecxal.code4guate.com/sonido.mp3');
-			audio.play();
-			document.getElementById("mensaje").innerHTML="<h1>Alarma sonando</h1>";
-			// al finalizar el sonido escondemos el mensaje de "Alarma sonando"
-			audio.onended = function() {
-				document.getElementById("mensaje").innerHTML = " ";
-				document.getElementById("hour").value = " ";
-				document.getElementById("minutes").value = " ";
-				document.getElementById("seconds").value = " ";
-			};
-		}
-	},
-1000);
+const setAlarmBtn = document.getElementById('set-alarm-btn');
+const alarmHourInput = document.getElementById('hour');
+const alarmMinuteInput = document.getElementById('minutes');
+const alarmSecondInput = document.getElementById('seconds');
+const alarmTypeInput = document.getElementById('type');
+
+setAlarmBtn.addEventListener('click', function() {
+  var date = new Date();
+  var currentHour = date.getHours();
+  var currentMinute = date.getMinutes();
+  var currentSecond = date.getSeconds();
+  var typeHour = currentHour < 12 ? 'AM' : 'PM';
+  currentSecond = (currentSecond < 10 ? '0' : '') + currentSecond;
+  currentMinute = (currentMinute < 10 ? '0' : '') + currentMinute;
+  currentHour = currentHour > 12 ? currentHour - 12 : currentHour;
+  currentHour = (currentHour < 10 ? '0' : '')
+  + currentHour;
+
+  var selectedHour = parseInt(alarmHourInput.value);
+  var selectedMinute = parseInt(alarmMinuteInput.value);
+  var selectedSecond = parseInt(alarmSecondInput.value);
+  var selectedType = alarmTypeInput.value;
+
+  if (
+    selectedHour === currentHour &&
+    selectedMinute === currentMinute &&
+    selectedSecond === currentSecond &&
+    selectedType === typeHour
+  ) {
+    var audio = new Audio('http://ecxal.code4guate.com/sonido.mp3');
+    audio.play();
+    document.getElementById('mensaje').innerHTML = '<h1>Alarma sonando</h1>';
+    audio.onended = function() {
+      document.getElementById('mensaje').innerHTML = ' ';
+      alarmHourInput.value = '';
+      alarmMinuteInput.value = '';
+      alarmSecondInput.value = '';
+    };
+  }
+});
